87e09aa4c4997edf9f84f49fdd1d2fa3bff8d1d3
Liz Tucker <ltucker284@gmail.com>
2019-10-25 11:14:35 -0400

    network start scripts properly boot up network

--- basic-network/joinChannel.sh
+++ basic-network/joinChannel.sh
@@ -4,4 +4,4 @@
 export CORE_PEER_LOCALMSPID=Org1MSP
 export CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp
 peer channel fetch oldest mychannel.block -c mychannel --orderer orderer.example.com:7050
-exit
+peer channel join -b mychannel.block
--- basic-network/start.sh
+++ basic-network/start.sh
@@ -25,6 +25,9 @@ sleep ${FABRIC_START_TIMEOUT}
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
-docker exec -it peer1.org1.example.com
-# Time for our peers to join
-#sh ./joinPeers.sh
+docker exec -i peer1.org1.example.com bash < joinChannel.sh
+docker exec -i peer2.org1.example.com bash < joinChannel.sh
+docker exec -i peer3.org1.example.com bash < joinChannel.sh
+docker exec -i peer4.org1.example.com bash < joinChannel.sh
+docker exec -i peer5.org1.example.com bash < joinChannel.sh
+docker exec -i peer6.org1.example.com bash < joinChannel.sh
