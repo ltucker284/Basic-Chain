53a183fdb4aa53511eccb30b4ed1af744bb4f41e
Liz Tucker <ltucker284@gmail.com>
2019-10-28 14:09:03 -0400

    removing more os dependent files

--- basic-network/generate.sh
+++ basic-network/generate.sh
@@ -12,31 +12,31 @@ CHANNEL_NAME=mychannel
 rm -fr config/*
 rm -fr crypto-config/*
 echo Removed cryptoconfig material
-
-# generate crypto material
-cryptogen generate --config=./crypto-config.yaml
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate crypto material..."
-  exit 1
-fi
-
-# generate genesis block for orderer
-configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate orderer genesis block..."
-  exit 1
-fi
-
-# generate channel configuration transaction
-configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate channel configuration transaction..."
-  exit 1
-fi
-
-# generate anchor peer transaction
-configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update for Org1MSP..."
-  exit 1
-fi
+#
+## generate crypto material
+#cryptogen generate --config=./crypto-config.yaml
+#if [ "$?" -ne 0 ]; then
+#  echo "Failed to generate crypto material..."
+#  exit 1
+#fi
+#
+## generate genesis block for orderer
+#configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+#if [ "$?" -ne 0 ]; then
+#  echo "Failed to generate orderer genesis block..."
+#  exit 1
+#fi
+#
+## generate channel configuration transaction
+#configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+#if [ "$?" -ne 0 ]; then
+#  echo "Failed to generate channel configuration transaction..."
+#  exit 1
+#fi
+#
+## generate anchor peer transaction
+#configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+#if [ "$?" -ne 0 ]; then
+#  echo "Failed to generate anchor peer update for Org1MSP..."
+#  exit 1
+#fi
