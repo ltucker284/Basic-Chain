559573cb316bc5c0c69a3f8dba7d58f1af9a4fd5
Liz Tucker <ltucker284@gmail.com>
2019-11-15 11:42:29 -0500

    all peers can endorse transactions

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -24,13 +24,17 @@ echo Create the channel
 sudo docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 echo Join peer0.org1.example.com to the channel.
 sudo docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
-echo Install and instantiate chaincode
+echo Install chaincode on peer0 and instantiate chaincode on channel
 sudo docker exec -i cli peer chaincode install -n mycc -v 1.0 -p "/opt/gopath/src/github.com/newcc" -l "node"
 sudo docker exec -i cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc -l "node" -v 1.0 -c '{"Args":[]}'
 
+echo Let the rest of the peers join the channel
 docker exec -i peer1.org1.example.com bash < ./setup-scripts/joinChannel.sh
 docker exec -i peer2.org1.example.com bash < ./setup-scripts/joinChannel.sh
 docker exec -i peer3.org1.example.com bash < ./setup-scripts/joinChannel.sh
 docker exec -i peer4.org1.example.com bash < ./setup-scripts/joinChannel.sh
 docker exec -i peer5.org1.example.com bash < ./setup-scripts/joinChannel.sh
 docker exec -i peer6.org1.example.com bash < ./setup-scripts/joinChannel.sh
+
+echo Install chaincode on the rest of the peers
+sh ./setup-scripts/installChaincode.sh
