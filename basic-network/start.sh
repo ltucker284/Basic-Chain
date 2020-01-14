46d7bb5aa314f2527d03e8a717d3361f65962db8
Liz Tucker <ltucker284@gmail.com>
2020-01-14 18:54:58 -0500

    all seven peers on the network

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -12,7 +12,7 @@ export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer1.org1.example.com couchdb1 
+docker-compose -f docker-compose.yml up -d peer6.org1.example.com
 docker ps -a
 
 # wait for Hyperledger Fabric to start
